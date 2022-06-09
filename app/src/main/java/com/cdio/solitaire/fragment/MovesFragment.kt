3155ef6ea5d6623cb45993dba060bba650126c04
Andreddi <andreas.carlsen98@gmail.com>
2022-06-09 18:34:39 +0200

    Added inputfield, used to type in cards which can't be recognized by the program

--- app/src/main/java/com/cdio/solitaire/fragment/MovesFragment.kt
+++ app/src/main/java/com/cdio/solitaire/fragment/MovesFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.navigation.Navigation
 import com.cdio.solitaire.R
+import com.google.android.material.textfield.TextInputLayout
 
 class MovesFragment : Fragment() {
 
     private var moveText: TextView? = null
 
     private var button: Button? = null
+
+    private var inputField: TextInputLayout? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,6 +32,9 @@ class MovesFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         moveText = view.findViewById(R.id.move_text)
+        
+        inputField = view.findViewById(R.id.input_field)
+        inputField?.isVisible = false
 
         button = view.findViewById(R.id.open_camera_button)
         button?.setOnClickListener { (navigateToCamera(view)) }
@@ -39,13 +46,13 @@ class MovesFragment : Fragment() {
         Navigation.findNavController(view).navigate(R.id.action_moves_to_camera)
     }
 
-    private fun changeNextMove(str : String){
+    private fun changeNextMoveText(str : String){
         moveText?.text = str
     }
 
     private fun getNextMove(view: View) {
         val nextMove = GameController.getLastMove()
-        changeNextMove(nextMove.toString())
+        changeNextMoveText(nextMove.toString())
         if(nextMove.NewPictureNeeded){
             flipButton(view)
         }
@@ -54,5 +61,31 @@ class MovesFragment : Fragment() {
     private fun flipButton(view: View){
         button?.text = getString(R.string.open_camera)
         button?.setOnClickListener { navigateToCamera(view)}
+    }
+
+    // If a card can't be recognized, make the user input the card
+    private fun getCardInput(str: String, view: View){
+        changeNextMoveText(str)
+
+        button?.text = getString(R.string.ok)
+
+        inputField?.isVisible = true
+
+        button?.setOnClickListener {
+            val card = inputField?.editText?.text.toString()
+
+            if(card != ""){
+
+                //TODO: Do something with the card
+
+                inputField?.isVisible = false
+                button?.text = getString(R.string.next_move)
+
+                // Since card input will only happen after taking a photo, get the next move
+                button?.setOnClickListener {
+                    getNextMove(view)
+                }
+            }
+        }
     }
 }
