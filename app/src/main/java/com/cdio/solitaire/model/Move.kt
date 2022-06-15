3fde60ee6aef33f70e3f1de369a6a481d97ca1a6
Andreddi <andreas.carlsen98@gmail.com>
2022-06-15 11:58:19 +0200

    Added toString method, and ctr+alt+L formatting

--- app/src/main/java/com/cdio/solitaire/model/Move.kt
+++ app/src/main/java/com/cdio/solitaire/model/Move.kt
@@ -12,16 +12,41 @@ enum class MoveType {
 }
 
 
-class Move(val moveType: MoveType,val sourceStack: CardStack? =null,var targetStack: CardStack?= null,val sourceCard: Card?= null) { // Needed for toString() method, and for checking validity
-    var cardToUpdate: Card?= null
-    var prev:Move?=null
-    var next:Move?=null
+class Move(
+    val moveType: MoveType,
+    val sourceStack: CardStack? = null,
+    var targetStack: CardStack? = null,
+    val sourceCard: Card? = null,
+) { // Needed for toString() method, and for checking validity
+    var cardToUpdate: Card? = null
+    var prev: Move? = null
+    var next: Move? = null
     fun toStringDanish(): String {
         return when (moveType) {
             MoveType.MOVE_STACK, MoveType.MOVE_FROM_TALON, MoveType.MOVE_FROM_FOUNDATION -> sourceCard!!.toStringDanish() + "-" + targetStack!!.stackID + ","
             MoveType.MOVE_TO_FOUNDATION -> sourceCard!!.rank.ordinal.toString() + sourceCard.suit.shortDanish() + "-F,"
             MoveType.DRAW_STOCK -> "T,"
             MoveType.FLIP_TALON -> "S,"
+            else -> ""
+        }
+    }
+
+    override fun toString(): String {
+        return when (moveType) {
+            MoveType.MOVE_STACK, MoveType.MOVE_FROM_TALON, MoveType.MOVE_FROM_FOUNDATION -> "Move " + sourceCard!!.toString() + " to " + getCardStackName(
+                targetStack!!.stackID
+            ) + targetStack!!.stackID
+            MoveType.MOVE_TO_FOUNDATION -> "Move " + sourceCard!!.toString() + "to foundation"
+            MoveType.DRAW_STOCK -> "Draw cards from stock"
+            MoveType.FLIP_TALON -> "Flip talon"
+            MoveType.DEAL_CARDS -> "Deal Cards"
+        }
+    }
+
+    private fun getCardStackName(id: Int): String {
+        return when (id) {
+            in 1..7 -> "Tableaux"
+            in 8..11 -> "Foundation"
             else -> ""
         }
     }
